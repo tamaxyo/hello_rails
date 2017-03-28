856685514645532ad46251cdfbf8704e940a3f8e
tamaxyo <tamaxyo@gmail.com>
2017-03-28 23:59:44 +0900

    move entry under blog

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,15 +1,18 @@
 class EntriesController < ApplicationController
   before_action :set_entry, only: [:show, :edit, :update, :destroy]
+  before_action :find_blog
 
   # GET /entries
   # GET /entries.json
   def index
-    @entries = Entry.all
+    #    @entries = Entry.where(:blog_id => @blog.id)
+    redirect_to blog_path(@blog)
   end
 
   # GET /entries/1
   # GET /entries/1.json
   def show
+    redirect_to blog_path(@blog) unless @blog.id == @entry.blog_id
   end
 
   # GET /entries/new
@@ -25,11 +28,12 @@ class EntriesController < ApplicationController
   # POST /entries.json
   def create
     @entry = Entry.new(entry_params)
+    @entry.blog = @blog if @blog
 
     respond_to do |format|
       if @entry.save
-        format.html { redirect_to @entry, notice: 'Entry was successfully created.' }
-        format.json { render :show, status: :created, location: @entry }
+        format.html { redirect_to [@blog, @entry], notice: 'Entry was successfully created.' }
+        format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new }
         format.json { render json: @entry.errors, status: :unprocessable_entity }
@@ -42,8 +46,8 @@ class EntriesController < ApplicationController
   def update
     respond_to do |format|
       if @entry.update(entry_params)
-        format.html { redirect_to @entry, notice: 'Entry was successfully updated.' }
-        format.json { render :show, status: :ok, location: @entry }
+        format.html { redirect_to @blog, notice: 'Entry was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
         format.json { render json: @entry.errors, status: :unprocessable_entity }
@@ -56,7 +60,7 @@ class EntriesController < ApplicationController
   def destroy
     @entry.destroy
     respond_to do |format|
-      format.html { redirect_to entries_url, notice: 'Entry was successfully destroyed.' }
+      format.html { redirect_to blog_entries_url, notice: 'Entry was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -70,5 +74,9 @@ class EntriesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def entry_params
       params.require(:entry).permit(:title, :body)
+    end
+
+    def find_blog
+      @blog = Blog.find(params[:blog_id])
     end
 end
