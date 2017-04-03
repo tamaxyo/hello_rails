a1d06140acb9e89b65b37b2aa484828279b2902b
tamaxyo <tamaxyo@gmail.com>
2017-04-03 21:57:24 +0900

    move resource comment under entry

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :blogs do
-    resources :entries
+    resources :entries do
+      resources :comments
+    end
   end
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
