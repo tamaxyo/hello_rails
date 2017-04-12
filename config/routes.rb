7fac073ad8016eee5bdb84333cda76225253fcff
tamaxyo <tamaxyo@gmail.com>
2017-04-13 08:55:11 +0900

    set root to blogs#index

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       end
     end
   end
-  
+
+  root 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
